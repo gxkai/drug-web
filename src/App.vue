b5c04ef11a992cbab8bb97a917fb1fa21d53e479
Mr.Wei <395349115@qq.com>
2018-07-19 11:10:29 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <span class="is-size-5">sadkladf</span>
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+@import '../static/css/bulma.css';
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
