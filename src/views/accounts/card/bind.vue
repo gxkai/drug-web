8910f881213a1b9a67360c56fc622be36f74614c
heyglacier <2284711614@qq.com>
2018-07-23 09:04:02 +0800

    fix

--- src/views/accounts/card/bind.vue
+++ src/views/accounts/card/bind.vue
@@ -1,77 +1,154 @@
 <template>
-  <div class="f_body">
+  <!--<div class="f_body">-->
+    <!--<mt-header title="医保卡绑定">-->
+      <!--<router-link to="/accounts" slot="left">-->
+        <!--<mt-button icon="back"></mt-button>-->
+      <!--</router-link>-->
+    <!--</mt-header>-->
+
+    <!--<div class="f_medical_card_bin_lists">-->
+      <!--<div class="f_medical_card_bin_list">-->
+        <!--<i class="icon iconfont ic-xingming" id="ic_xingming"></i>-->
+        <!--<input type="text" v-model="account.name" placeholder="请先维护账户信息" disabled/>-->
+      <!--</div>-->
+
+      <!--<div class="f_medical_card_bin_list">-->
+      <!--<i class="icon iconfont ic-Id" id="ic_Id"></i>-->
+        <!--<input type="number" v-model="account.identityNumber" placeholder="请先维护账户信息" disabled/>-->
+      <!--</div>-->
+
+      <!--<div class="f_medical_card_bin_list">-->
+      <!--<i class="icon iconfont ic-shouji" id="ic_shouji"></i>-->
+        <!--<input type="number" v-model="account.username" placeholder="请输入手机号码" disabled/>-->
+      <!--</div>-->
+
+      <!--<div class="f_medical_card_bin_list">-->
+          <!--<i class="icon iconfont ic-Id" id="ic_yibaocard"></i>-->
+        <!--<input type="number" v-model="account.medicalNumber" placeholder="请输入医保卡编号"/>-->
+      <!--</div>-->
+    <!--</div>-->
+
+    <!--<div class="f_medical_card_bin_button">-->
+      <!--<button @click="bind()">确认绑定</button>-->
+    <!--</div>-->
+  <!--</div>-->
+
+  <div class="content">
     <mt-header title="医保卡绑定">
       <router-link to="/accounts" slot="left">
         <mt-button icon="back"></mt-button>
       </router-link>
     </mt-header>
 
-    <div class="f_medical_card_bin_lists">
-      <div class="f_medical_card_bin_list">
-        <i class="icon iconfont ic-xingming" id="ic_xingming"></i>
-        <input type="text" v-model="account.name" placeholder="请先维护账户信息" disabled/>
+    <nav class="panel is-mobile">
+      <p class="panel-heading">
+        repositories
+      </p>
+      <div class="panel-block">
+        <p class="control has-icons-left">
+          <input class="input is-small" type="text" placeholder="search">
+          <span class="icon is-small is-left">
+        <i class="fas fa-search"></i>
+      </span>
+        </p>
       </div>
-
-      <div class="f_medical_card_bin_list">
-      <i class="icon iconfont ic-Id" id="ic_Id"></i>
-        <input type="number" v-model="account.identityNumber" placeholder="请先维护账户信息" disabled/>
+      <p class="panel-tabs">
+        <a class="is-active">all</a>
+        <a>public</a>
+        <a>private</a>
+        <a>sources</a>
+        <a>forks</a>
+      </p>
+      <a class="panel-block is-active">
+    <span class="panel-icon">
+      <i class="fas fa-book"></i>
+    </span>
+        bulma
+      </a>
+      <a class="panel-block">
+    <span class="panel-icon">
+      <i class="fas fa-book"></i>
+    </span>
+        marksheet
+      </a>
+      <a class="panel-block">
+    <span class="panel-icon">
+      <i class="fas fa-book"></i>
+    </span>
+        minireset.css
+      </a>
+      <a class="panel-block">
+    <span class="panel-icon">
+      <i class="fas fa-book"></i>
+    </span>
+        jgthms.github.io
+      </a>
+      <a class="panel-block">
+    <span class="panel-icon">
+      <i class="fas fa-code-branch"></i>
+    </span>
+        daniellowtw/infboard
+      </a>
+      <a class="panel-block">
+    <span class="panel-icon">
+      <i class="fas fa-code-branch"></i>
+    </span>
+        mojs
+      </a>
+      <label class="panel-block">
+        <input type="checkbox">
+        remember me
+      </label>
+      <div class="panel-block">
+        <button class="button is-link is-outlined is-fullwidth">
+          reset all filters
+        </button>
       </div>
+    </nav>
 
-      <div class="f_medical_card_bin_list">
-      <i class="icon iconfont ic-shouji" id="ic_shouji"></i>
-        <input type="number" v-model="account.username" placeholder="请输入手机号码" disabled/>
-      </div>
 
-      <div class="f_medical_card_bin_list">
-          <i class="icon iconfont ic-Id" id="ic_yibaocard"></i>
-        <input type="number" v-model="account.medicalNumber" placeholder="请输入医保卡编号"/>
-      </div>
-    </div>
-
-    <div class="f_medical_card_bin_button">
-      <button @click="bind()">确认绑定</button>
-    </div>
   </div>
+
 </template>
 
 <script>
-import {MessageBox} from 'mint-ui';
-import exception from 'static/js/exception';
+// import {MessageBox} from 'mint-ui';
+// import exception from 'static/js/exception';
 
 export default {
   data() {
     return {
       account: {}
     };
-  },
-  created() {
-    this.account = this.$store.getters.account;
-  },
-  methods: {
-    bind: function () {
-      if (this.account.medicalNumber === '' || this.account.medicalNumber == null) {
-        MessageBox('提示', '医保不能为空');
-        return;
-      }
-      this.$http.put(this.URL_PATH + '/accounts', this.account, {
-        headers: {
-          'Authorization': this.$store.getters.token
-        }
-      })
-        .then((res) => {
-          this.$store.commit('setAccount', this.account);
-          this.$router.push({
-            name: '/accounts/bind/success',
-            params: {
-              bind: 1
-            }
-          });
-        })
-        .catch((error) => {
-          exception.message(error);
-        });
-    }
   }
+  // created() {
+  //   this.account = this.$store.getters.account;
+  // },
+  // methods: {
+  //   bind: function () {
+  //     if (this.account.medicalNumber === '' || this.account.medicalNumber == null) {
+  //       MessageBox('提示', '医保不能为空');
+  //       return;
+  //     }
+  //     this.$http.put(this.URL_PATH + '/accounts', this.account, {
+  //       headers: {
+  //         'Authorization': this.$store.getters.token
+  //       }
+  //     })
+  //       .then((res) => {
+  //         this.$store.commit('setAccount', this.account);
+  //         this.$router.push({
+  //           name: '/accounts/bind/success',
+  //           params: {
+  //             bind: 1
+  //           }
+  //         });
+  //       })
+  //       .catch((error) => {
+  //         exception.message(error);
+  //       });
+  //   }
+  // }
 };
 </script>
 
