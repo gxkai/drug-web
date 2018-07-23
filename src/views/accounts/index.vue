29715fcd8c78dc144ef7f957c27f9f3c85b03d97
heyglacier <2284711614@qq.com>
2018-07-23 16:44:55 +0800

    fix

--- src/views/accounts/index.vue
+++ src/views/accounts/index.vue
@@ -1,11 +1,9 @@
 <template>
   <!--<div class="f_body accountbody">-->
-
     <!--<mt-header>-->
       <!--<router-link to="/messageTypes" slot="left">-->
         <!--<i class="icon iconfont ic-xiaoxi"></i>-->
       <!--</router-link>-->
-
       <!--<router-link to="/setting" slot="right">-->
         <!--<i class="icon iconfont ic-shezhi1"></i>-->
       <!--</router-link>-->
@@ -99,54 +97,70 @@
     </div>
     <div class="nav-bar ">
       <div class="nav-bar-title flex-stream-sb">
-        <span>全部订单</span>
+        <span class="all-orders">全部订单</span>
         <div class="is-flex">
           <span>我的订单</span>
-          <i class="icon" />
+          <i class="iconfont ic-youjiantou" />
         </div>
       </div>
+      <div class="nav-bar-content border-top-gray">
+        <router-link class="flex-box nav-bar-content-margin" to="/drugAppraises">
+          <img src="../../assets/image/accounts/evaluation.png"/>
+          <span>代付款</span>
+        </router-link>
 
-      <hr class="text-hr" >
-
-    </div>
-    <div class="account-content is-flex">
-        <router-link class="flex-box" to="/drugAppraises">
+        <router-link class="flex-box nav-bar-content-margin" to="/drugAppraises">
           <img src="../../assets/image/accounts/evaluation.png"/>
           <span>我的评价</span>
         </router-link>
-        <router-link class="flex-box" to="/chats">
+        <router-link class="flex-box nav-bar-content-margin" to="/drugAppraises">
+          <img src="../../assets/image/accounts/evaluation.png"/>
+          <span>我的评价</span>
+        </router-link>
+        <router-link class="flex-box nav-bar-content-margin" to="/drugAppraises">
+          <img src="../../assets/image/accounts/evaluation.png"/>
+          <span>我的评价</span>
+        </router-link>
+      </div>
+    </div>
+    <div class="account-content is-flex">
+        <router-link class="flex-box account-content-padding border-right-gray border-bottom-gray" to="/drugAppraises">
+          <img src="../../assets/image/accounts/evaluation.png"/>
+          <span class="mt-15">我的评价</span>
+        </router-link>
+        <router-link class="flex-box account-content-padding border-right-gray border-bottom-gray" to="/chats">
           <img src="../../assets/image/accounts/consultation.png"/>
-          <span>咨询</span>
+          <span class="mt-15">咨询</span>
         </router-link>
-        <router-link class="flex-box" to="/addresses" >
+        <router-link class="flex-box account-content-padding border-right-gray border-bottom-gray" to="/addresses" >
           <img src="../../assets/image/accounts/address.png"/>
-          <span>地址</span>
+          <span class="mt-15">地址</span>
         </router-link>
-        <router-link class="flex-box" to="/collects" >
+        <router-link class="flex-box account-content-padding border-bottom-gray" to="/collects" >
           <img src="../../assets/image/accounts/coll.png"/>
-          <span>收藏</span>
+          <span class="mt-15">收藏</span>
         </router-link>
-        <router-link class="flex-box" to="/orderRefunds">
+        <router-link class="flex-box account-content-padding border-right-gray" to="/orderRefunds">
           <img src="../../assets/image/accounts/refund.png"/>
-          <span>退款</span>
+          <span class="mt-15">退款</span>
         </router-link>
-        <!--<router-link class="flex-box"-->
-        <!--:to="null==this.account.medicalNumber?'/accounts/bind':'/accounts/unbind'" style="margin-top:0px;border-bottom:0px;" >-->
-        <!--<img src="../../assets/image/accounts/bind.png"/>-->
-        <!--<span>账户信息</span>-->
-        <!--</router-link>-->
-        <router-link class="flex-box" to="/accounts/view">
+        <router-link class="flex-box account-content-padding border-right-gray" to="/" >
+                     <!--:to="null==this.account.medicalNumber?'/accounts/bind':'/accounts/unbind'"-->
+        <img src="../../assets/image/accounts/bind.png"/>
+        <span class="mt-15">账户信息</span>
+        </router-link>
+        <router-link class="flex-box account-content-padding border-right-gray" to="/accounts/view">
           <img src="../../assets/image/accounts/card.png"/>
-          <span>医保卡信息</span>
+          <span class="mt-15">医保卡信息</span>
         </router-link>
     </div>
     <div class="foot-tag flex-row-sb-center">
       <span >常见问题</span>
-      <i class="icon" />
+      <i class="iconfont ic-youjiantou" />
     </div>
     <div class="foot-tag flex-row-sb-center">
       <span >意见反馈</span>
-      <i class="icon" />
+      <i class="iconfont ic-youjiantou" />
     </div>
 
   </div>
@@ -208,6 +222,11 @@
     color: #e2e2e2;
   }
 
+  .is-flex{
+    display: flex !important;
+  }
+
+
   .border-left-gray{
     border-left:1px rgba(235,235,235,1) solid;
   }
@@ -221,6 +240,10 @@
     border-bottom: 1px rgba(235,235,235,1) solid;
   }
 
+  .account-container{
+    background: #f5f5f5;
+    height: 100vh;
+  }
   .all-center{
     display: flex;
     flex-direction: column;
@@ -271,14 +294,26 @@
     width:719px;
     height:154px;
     background:rgba(255,255,255,1);
-    border: 1px black solid;
+    margin-bottom: 14px;
   }
-
-  .text-hr{
-    width:690px;
-    height:1px;
-    opacity:0.38;
-    border:1px rgba(204,204,204,1) solid;
+  .nav-bar-title{
+    box-sizing: border-box;
+    padding: 5px 15px;
+  }
+  .nav-bar-content-margin{
+    margin-top: 15px;
+  }
+  .all-orders{
+    width:120px;
+    font-size:24px;
+    font-family:HiraginoSansGB-W3;
+    color:rgba(51,51,51,1);
+    line-height:40px;
+  }
+  .nav-bar-content{
+    display: flex;
+    flex-direction: row;
+    margin-bottom: 14px;
   }
 
   .account-content{
@@ -289,22 +324,25 @@
   }
 
   .flex-box{
-    padding: 29px 48px;
     box-sizing: border-box;
     width:25%;
     display: flex;
     flex-direction: column;
     align-items: center;
-    border:
+  }
+  .account-content-padding{
+    padding: 29px 48px;
   }
   .flex-box span{
-    width:100px;
+    width:120px;
     height:20px;
     font-size:20px;
     font-family:MicrosoftYaHei;
     color:rgba(51,51,51,1);
     line-height:36px;
     text-align: center;
+  }
+  .mt-15{
     margin-top: 15px;
   }
   .flex-box img{
@@ -323,153 +361,4 @@
     font-family:HiraginoSansGB-W3;
     color:rgba(102,102,102,1);
   }
-
-  /*.f_account_center_order .icon .mint-badge {*/
-    /*position: absolute !important;*/
-    /*right: -13% !important;*/
-    /*top: -13% !important;*/
-    /*height: 1.5rem !important;*/
-    /*width: 1.5rem !important;*/
-    /*font-size: 0.5rem !important;*/
-    /*border-radius: 1.5rem !important;*/
-    /*margin: 0 !important;*/
-    /*text-align: center !important;*/
-    /*line-height: 1.5rem !important;*/
-    /*padding: 0 !important;*/
-  /*}*/
-
-  /*.f_body {*/
-    /*width: 100%;*/
-    /*height: 100%;*/
-    /*background: #f5f5f5;*/
-    /*color: #757575;*/
-    /*text-align: left;*/
-  /*}*/
-
-  /*.f_account_center_header {*/
-    /*width: 100%;*/
-    /*text-align: center;*/
-    /*box-sizing: border-box;*/
-    /*padding: 2rem;*/
-    /*background: #12b1f9;*/
-    /*color: #e1f4f5;*/
-  /*}*/
-
-  /*.f_account_center_header img {*/
-    /*width: 4rem;*/
-    /*height: 4rem;*/
-    /*border-radius: 50%;*/
-  /*}*/
-
-  /*.f_account_center_header > span {*/
-    /*display: block;*/
-  /*}*/
-
-  /*.f_account_center_order .icon {*/
-    /*font-size: 2.5rem;*/
-    /*position: relative;*/
-  /*}*/
-
-  /*.f_account_center_orders {*/
-    /*width: 100%;*/
-    /*display: flex;*/
-    /*flex-direction: row;*/
-    /*justify-content: space-between;*/
-    /*box-sizing: border-box;*/
-    /*padding: 1.2rem 1rem;*/
-    /*background: white;*/
-    /*border-bottom: 2px #f9f9f9 solid;*/
-  /*}*/
-
-  /*.f_account_center_orders :last-child {*/
-    /*font-size: 1rem;*/
-    /*color: #a5a5a5;*/
-  /*}*/
-
-  /*.f_account_my_orders {*/
-    /*display: flex;*/
-    /*justify-items: center*/
-  /*}*/
-
-  /*.f_account_center_order_lists {*/
-    /*box-sizing: border-box;*/
-    /*padding-bottom: 0.5rem;*/
-    /*width: 100%;*/
-    /*display: flex;*/
-    /*flex-wrap: wrap;*/
-    /*background: white;*/
-  /*}*/
-
-  /*.f_account_center_menu_lists {*/
-    /*width: 100%;*/
-    /*display: flex;*/
-    /*flex-wrap: wrap;*/
-    /*background: white;*/
-    /*margin-top: 0.5rem;*/
-    /*height: 15rem;*/
-  /*}*/
-
-
-   /*.f_account_center_menu_list {*/
-    /*width: 25%;*/
-    /*margin-top: 0.5rem;*/
-    /*display: flex;*/
-    /*flex-direction: column;*/
-    /*justify-content: center;*/
-    /*align-items: center;*/
-    /*position: relative;*/
-
-  /*}*/
-
-  /*.f_account_center_menu_listd {*/
-    /*width: 25%;*/
-    /*padding-top: 0.5rem;*/
-    /*display: flex;*/
-    /*flex-direction: column;*/
-    /*justify-content: center;*/
-    /*align-items: center;*/
-    /*position: relative;*/
-    /*border-right: 1px solid #ebebeb;*/
-    /*border-bottom: 1px solid #ebebeb;*/
-  /*}*/
-  /*.f_account_center_menu_listd:nth-child(4n){*/
-    /*border-right: none;*/
-  /*}*/
-
-  /*.f_account_center_menu_list img {*/
-    /*width: 2rem;*/
-    /*height: 2rem;*/
-  /*}*/
-
-  /*.f_account_center_menu_list span {*/
-    /*display: block;*/
-    /*font-size: 1rem;*/
-    /*margin-top: 0.5rem;*/
-  /*}*/
-
-  /*.f_account_center_questions, .f_account_center_feedback {*/
-    /*margin-top: 0.5rem;*/
-    /*background: white;*/
-    /*padding: 1rem;*/
-    /*box-sizing: border-box;*/
-    /*display: flex;*/
-    /*color: #969696;*/
-    /*font-size: 1rem;*/
-    /*justify-content: space-between;*/
-  /*}*/
-
-  /*.ic-xiaoxi, .ic-shezhi1 {*/
-    /*color: white;*/
-    /*font-size: 2rem;*/
-  /*}*/
-
-  /*.mint-badge.is-size-small {*/
-    /*height: 15px;*/
-    /*padding: 0px;*/
-    /*width: 15px;*/
-  /*}*/
-
-  /*.accountbody {*/
-    /*height: 100vh;*/
-  /*}*/
 </style>
