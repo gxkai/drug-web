8f42ed2bb4d638053c944223f0b562df4aa98a8d
gxkai <1378026744@qq.com>
2018-07-27 17:34:34 +0800

    购物车

--- src/assets/js/component.js
+++ src/assets/js/component.js
@@ -5,6 +5,7 @@ import newNoData from '../../components/no-data';
 import newEditDiv from '../../components/edit-div';
 import newEditLine from '../../components/edit-line';
 import newMain from '../../components/main';
+import newLeftSilde from '../../components/left-slider';
 
 export default {install(Vue, options) {
   Vue.component('new-header', newHeader);
@@ -14,4 +15,5 @@ export default {install(Vue, options) {
   Vue.component('new-edit-div', newEditDiv);
   Vue.component('new-edit-line', newEditLine);
   Vue.component('new-main', newMain);
+  Vue.component('new-left-slide', newLeftSilde);
 }};
