42810e42c56de5edb2071b9693d958e04dbd36d7
gxkai <1378026744@qq.com>
2018-08-06 13:03:47 +0800

    退款申请

--- src/views/orderRefunds/create.vue
+++ src/views/orderRefunds/create.vue
@@ -7,106 +7,33 @@
       </div>
     </new-header>
 
-    <ul class="">
-      <div v-for="rx in rxDrugs">
-        <li class="is-flex flex-sb refund-title p-lr-20">
-          <div>
-            <span>处方单</span>
-            <span><i class="iconfont icon-xiala"></i></span>
-          </div>
-          <router-link tag="div" :to="{path:'/rxs/view',query:{rxId:rx.rxId}}" class="J_ReturnGoods_check">查看处方>
-          </router-link>
-        </li>
-        <li>
-          <div v-for="drug in rx.drugs">
-            <refund-drug class="mb-10" :fileId="drug.logo" :drugName="drug.drugName" :spec="drug.spec"
-                         :price="drug.price" :quantity="drug.quantity"></refund-drug>
-          </div>
-        </li>
-      </div>
-
+    <new-refund :order="orderInfo"></new-refund>
+    <div class="refund-reason">
       <div>
-        <div class="is-flex flex-sb refund-title p-lr-20">
-          <span>非处方单</span>
-          <span><i class="iconfont ic-xiajiantou"></i></span>
-        </div>
-        <li class="d_li">
-          <div v-for="drug in norDrugs">
-            <refund-drug class="mb-10" :fileId="drug.logo" :drugName="drug.drugName" :spec="drug.spec"
-                         :price="drug.price" :quantity="drug.quantity"></refund-drug>
-          </div>
-        </li>
+        <span>退款原因</span>
       </div>
-    </ul>
-
-    <div class="refund-reason is-flex flex-sb p-lr-20">
-      <div>退款原因</div>
       <div>
-        <span @click="handleClick()">{{pleasechoose}}</span>
-        <span @click="handleClick()"><i class="iconfont ic-youjiantou"></i></span>
+        <span>请选择<i class="iconfont ic-youjiantou"></i></span>
       </div>
     </div>
-
-    <div class="refund-price mt-10 is-flex flex-column flex-ju p-lr-20">
+    <div class="refund-price">
       <div>
-        <span>退款金额:</span>
-        <span class="text-red">￥{{price}}</span>
-      </div>
-      <div class="text-999999">
-        <span>最多</span><span>￥{{price}}，</span>
-        <span>含发货邮费</span><span>￥0.00</span>
+        <span>退款金额：</span>
+        <span>￥182.2</span>
       </div>
     </div>
-
-    <div class="refund-price mt-10 p-lr-20">
-      <span>退款说明:</span>
-      <span><input type="text" placeholder="选填" v-model="explain"></span>
-    </div>
-
-    <div class="refund-phone mt-10 p-lr-20">
-      <span>上传凭证</span>
-      <div v-if="flieBtn">
-        <div class="phone-box">
-          <div class="is-flex flex-column flex-ju flex-item">
-            <img src="../../assets/image/order/cam.png" class="is-37x32"/>
-            <span class="fz-12">上传凭证</span>
-            <span class="fz-12">最多三张</span>
-          </div>
-          <input type="file" @change="onUpload" id="file" multiple="Mmultiple" v-if="flieBtn"/>
-        </div>
+    <div class="refund-description">
+      <div>
+        <span>退款说明</span>
+      </div>
+      <div>
+        <textarea placeholder="选填 最多50字" maxlength="50" rows="3" >
+        </textarea>
       </div>
     </div>
-    <div id="result" name="result"></div>
-    <div class="btn-fixed" @click="commit">提交</div>
+    <div class="refund-image">
 
-    <mt-popup v-model="popupVisible" popup-transition="popup-fade" position="bottom" closeOnClickModal="true">
-      <div class="shadow-box">
-          <span>
-            <span class="ml-10"><b>退款原因</b></span>
-            <i class="iconfont ic-guanbi2 checkbox-group text-red"
-               @click="popupVisible=!popupVisible"></i>
-          </span>
-        <div class="mt-10">
-          <ul>
-            <li class="m-lr-10">
-              多拍/拍错/不想要
-              <span class="checkbox-group">
-                      <input type="radio" name="fu" id="3" v-model="reason" value="多拍/拍错/不想要"/>
-                      <label for="3"></label>
-                  </span>
-            </li>
-            <li class="m-lr-10">
-              缺货
-              <span class="checkbox-group">
-                      <input type="radio" name="fu" id="4" v-model="reason" value="缺货"/>
-                      <label for="4"></label>
-                  </span>
-            </li>
-          </ul>
-        </div>
-        <mt-button class="btn-fixed" size="large" type="primary" @click="sure()">确认</mt-button>
-      </div>
-    </mt-popup>
+    </div>
 
   </div>
 </template>
@@ -118,11 +45,9 @@
     name: 'orderRefundsCreate',
     data() {
       return {
-        msg: '组件',
         value: '',
         popupVisible: false,
         flieBtn: true,
-        orderId: '',
         pleasechoose: '请选择',
         rxDrugs: [],
         norDrugs: [],
@@ -137,7 +62,8 @@
           '不想买了'
         ],
         fileId: '',
-        accountId: ''
+        orderId: this.$route.query.orderId,
+        account: this.$store.getters.account
       };
     },
     methods: {
@@ -154,14 +80,10 @@
           'orderId': this.orderId,
           'price': this.price,
           'reason': this.reason
-        }, {
-          headers: {
-            'Authorization': this.$store.getters.token
-          }
         }).then(res => {
-          if (res.status === 200) {
-            this.$router.push({path: '/orderRefunds/view', query: {id: res.data}});
-          }
+          this.$router.push({path: '/orderRefunds/view', query: {id: res.data}});
+        }).catch(error => {
+          this.exception(error);
         });
       },
       delete: function (index) {
@@ -210,149 +132,89 @@
       }
     },
     created() {
-      this.accountId = this.$store.getters.account.id;
-      this.orderId = this.$route.query.orderId;
-      this.$http.get('/orders/' + this.orderId, {
-        headers: {
-          'Authorization': this.$store.getters.token
-        }
-      })
+      this.$http.get('/orders/' + this.orderId)
         .then(res => {
           this.orderInfo = res.data;
-          if (res.data.rxDrugs) {
-            this.rxDrugs = res.data.rxDrugs;
-          }
-          if (res.data.norDrugs) {
-            this.norDrugs = res.data.norDrugs;
-          }
-          this.rxDrugs.forEach(rx => {
-            rx.drugs.forEach(drug => {
-              drug.logo = '/files/' + drug.fileId + '/image?resolution=LARGE_LOGO';
-            });
-          });
-          this.norDrugs.forEach(drug => {
-            drug.logo = '/files/' + drug.fileId + '/image?resolution=LARGE_LOGO';
-          });
-          this.price = this.orderInfo.totalAmount;
+        }).catch(error => {
+          this.exception(error);
         });
     }
   };
 </script>
 
 <style scoped>
-  .refund-container {
-    margin-bottom: 100px;
-  }
-
-  .checkbox-group {
-    float: right;
-  }
-
-  .is-37x32 {
-    width: 37px;
-    height: 32px;
-  }
-
-  .m-lr-10 {
-    margin-left: 10px;
-    margin-right: 10px;
-  }
-
-  .ml-10 {
-    margin-left: 10px !important;
-  }
-
-  .mb-10 {
-    margin-bottom: 10px !important;
-  }
-
-  .mt-10 {
-    margin-top: 10px !important;
-  }
-
-  .p-lr-20 {
-    padding-left: 20px;
-    padding-right: 20px;
-  }
-
-  .is-flex {
-    display: flex !important;
-  }
-
-  .flex-column {
-    flex-direction: column;
-  }
-
-  .flex-ju {
-    justify-content: center;
-  }
-
-  .flex-item {
-    align-items: center;
-  }
-
-  .flex-sb {
-    justify-content: space-between;
-  }
-
   .refund-reason {
-    width: 720px;
-    height: 60px;
-    line-height: 60px;
-    background: #f6f6f6;
+    width:719px;
+    height:60px;
+    background:rgba(246,246,246,1);
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: 15px;
+  }
+  .refund-reason div:nth-child(1) {
+    font-size:26px;
+    font-family:HiraginoSansGB-W3;
+    color:rgba(51,51,51,1);
+    margin-left: 21px;
+  }
+  .refund-reason div:nth-child(2) {
+    font-size:24px;
+    font-family:HiraginoSansGB-W3;
+    color:rgba(102,102,102,1);
   }
 
   .refund-price {
-    width: 720px;
+    width:719px;
+    height:60px;
+    background:rgba(246,246,246,1);
+    margin-top: 15px;
+    display: flex;
+    align-items: center;
+  }
+  .refund-price div:nth-child(1) {
+    margin-left: 21px;
+  }
+  .refund-price div:nth-child(1) span:nth-child(1) {
+    font-size:26px;
+    font-family:HiraginoSansGB-W3;
+    color:rgba(51,51,51,1);
+  }
+  .refund-price div:nth-child(1) span:nth-child(2) {
+    font-size:24px;
+    font-family:HiraginoSansGB-W3;
+    color:rgba(255,0,0,1);
+  }
+
+  .refund-description {
+    width:719px;
+    height:116px;
+    background:rgba(246,246,246,1);
+    margin-top: 15px;
+    display: flex;
+  }
+
+  .refund-description> div:nth-child(1) {
+    width: 131px;
     height: 116px;
-    background: #f6f6f6;
   }
 
-  .refund-price input {
-    outline: 0;
+  .refund-description> div:nth-child(1) span:nth-child(1) {
+    margin-left: 20px;
+    line-height:45px;
+  }
+
+  .refund-description> div:nth-child(2) {
+    width: calc(720px - 131px);
+    height: 116px;
+  }
+  .refund-description> div:nth-child(2) textarea {
+    width: calc(720px - 131px);
+    height: 116px;
+    background:rgba(246,246,246,1);
+    resize: none;
+    outline: none;
     border: 0;
-    background: #f6f6f6;
-  }
-
-  .refund-phone {
-    width: 720px;
-    height: 206px;
-    background: #f6f6f6;
-  }
-
-  .phone-box {
-    width: 147px;
-    height: 121px;
-    margin-top: 10px !important;
-  }
-
-  .fz-12 {
-    font-size: 8px !important;
-  }
-
-  /* 遮罩层 */
-  .shadow-box {
-    width: 720px;
-    height: 421px;
-  }
-
-  .refund-title {
-    height: 60px;
-    line-height: 60px;
-  }
-
-  .btn-fixed {
-    width: 720px;
-    height: 77px;
-    line-height: 77px;
-    text-align: center;
-    background: rgba(19, 193, 254, 1);
-    border-radius: 5px;
-    position: fixed;
-    bottom: 0;
-    left: 0;
-    border: 0;
-    color: white;
-    letter-spacing: 2px;
+    line-height:45px;
   }
 </style>
