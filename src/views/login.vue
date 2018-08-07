73b41d4534693519acd17b4b504c8e74e174f33a
DannaWU <1982911028@qq.com>
2018-08-07 17:19:42 +0800

    css修改

--- src/views/login.vue
+++ src/views/login.vue
@@ -47,60 +47,6 @@
       </swiper-slide>
     </swiper>
 
-
-    <swiper class="mid2" :options="swiperOption" style="display: none">
-      <swiper-slide class="slide1">
-        <!--<div class="input1">
-          <div class="left">
-            <i class="iconfont ic-shouji shouji"></i>
-          </div>
-          <input type="number" class="right" placeholder="请输入手机号码" v-model="username">
-        </div>
-        <div class="input2">
-          <div class="left">
-            <i class="iconfont ic-icon2 icon2"></i>
-          </div>
-          <div class="right">
-            <input class="input21" type="password" placeholder="请输入密码" v-model="password">
-          </div>
-        </div>
-        <button class="button1" @click="login()">
-          登录
-        </button>-->
-      </swiper-slide>
-      <swiper-slide class="slide2">4444
-        <!--<div class="input1">
-          <div class="left">
-            <i class="iconfont ic-shouji shouji"></i>
-          </div>
-          <div class="right">
-            <input type="number" class="input11" placeholder="请输入手机号码" v-model="registerUsername">
-            <button class="button1" @click="getCaptcha()" v-show="showCode">获取验证码</button>
-            <button class="button1" v-show="!showCode">{{count}}</button>
-          </div>
-        </div>
-        <div class="input2">
-          <div class="left">
-            <i class="iconfont ic-anquanrenzheng anquanrenzheng"></i>
-          </div>
-          <div class="right">
-            <input class="input21"  placeholder="请输入验证码" v-model="captcha">
-          </div>
-        </div>
-        <div class="input3">
-          <div class="left">
-            <i class="iconfont ic-xinmima xinmima"></i>
-          </div>
-          <div class="right">
-            <input class="input31" placeholder="请输入密码" type="password" v-model="registerPassword">
-          </div>
-        </div>
-        <button class="button2" @click="register()">
-          注册
-        </button>-->
-      </swiper-slide>
-      <div class="swiper-pagination" slot="pagination"></div>
-    </swiper>
     <router-link tag="div" class="foot1" to="/forget">忘记密码？</router-link>
   </div>
 </template>
