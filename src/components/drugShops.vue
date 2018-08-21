ec519231d05ba94f94e29e9a3c97a90f42729199
gxkai <1378026744@qq.com>
2018-08-21 13:14:34 +0800

    跳转失灵

--- src/components/drugShops.vue
+++ src/components/drugShops.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="refund-box is-flex flex-row flex-item p-lr-20 position-relative">
+  <div class="refund-box is-flex flex-row flex-item p-lr-20 position-relative"
+       @click="$router.push({path:'/drugs/shops',query:{id:item.id,drugId:item.drugId}})">
     <span class="toc-tip position-absolute all-center" v-if="item.otc === true">非</span>
     <span class="toc-tip position-absolute all-center bg-2BB292" v-else>处</span>
     <img class="is-200x200" v-lazy="getImgURL(item.fileId, 'LARGE_LOGO')">
