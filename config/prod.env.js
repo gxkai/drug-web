dd4a77ad267446397dd7e58fec46d14a9e04af26
Mr.Wei <395349115@qq.com>
2018-10-15 16:53:01 +0800

    prod配置文件提交

--- config/prod.env.js
+++ config/prod.env.js
@@ -3,8 +3,8 @@ module.exports = {
   NODE_ENV: '"production"',
   // API_ROOT: '"http://172.16.0.107:8081/api"',
   // OUTSIDE_ROOT: '"http://172.16.0.107:8083/api/outside"'
-  API_ROOT: '"http://58.210.169.168:8081/api"',
-  OUTSIDE_ROOT: '"http://58.210.169.168:8083/api/outside"',
+  API_ROOT: '"http://180.97.207.84:57601/app/api"',
+  OUTSIDE_ROOT: '"http://180.97.207.84:57601/outside/api/outside"',
   // API_ROOT: '"http://192.168.31.228:8081/api"',
   // OUTSIDE_ROOT: '"http://192.168.31.228:8083/api/outside"'
 }
