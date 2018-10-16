eb675487ee303ee5088975a99389d7d57b85b8b2
gxkai <1378026744@qq.com>
2018-10-16 09:45:53 +0800

    正式环境地址

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,6 +2,6 @@
 module.exports = {
   NODE_ENV: '"production"',
   EVN_CONFIG:'"prod"',
-  API_ROOT: '"http://58.210.169.168:8081/api"',
-  OUTSIDE_ROOT: '"http://58.210.169.168:8083/api/outside"',
+  API_ROOT: '"http://180.97.207.84:57601/app/api"',
+  OUTSIDE_ROOT: '"http://180.97.207.84:57601/outside/api/outside"',
 }
