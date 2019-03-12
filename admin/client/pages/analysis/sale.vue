eebe972e553e33165bac31f21ca6c9deb2246fda
TulipaGesneriana <503026694@qq.com>
2019-03-12 08:29:41 +0800

    销售分析

--- admin/client/pages/analysis/sale.vue
+++ admin/client/pages/analysis/sale.vue
@@ -1,81 +1,65 @@
 <template>
-  <div>
-    <div>
-      <d2-crud
-        :columns="columns"
-        :data="data"
-        :loading="loading"
-        :pagination="pagination"
-        :options="options"
-        :rowHandle="rowHandle"
-        @custom-emit-1="handleCustomEvent"
-       />
+  <div class="sale-wrap">
+    <div class="search">
+      <el-select v-model="value8" filterable placeholder="请选择">
+        <el-option
+          v-for="item in list"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value">
+        </el-option>
+      </el-select>
+      <el-button type="primary">搜索</el-button>
+      <el-button>清空</el-button>
+    </div>
+
+    <div class="sales">
     </div>
   </div>
 </template>
 <script>
   import Vue from 'vue'
   import Component from 'class-component'
+  import PieDemo from '@/components/examples/charts/PieDemo'
 
-  @Component
-  export default class Warning extends Vue {
-    columns= [
-      {
-        title: '卡密',
-        key: 'key',
-        width: 320
-      },
-      {
-        title: '面值',
-        key: 'value'
-      },
-      {
-        title: '管理员',
-        key: 'admin'
-      },
-      {
-        title: '创建时间',
-        key: 'dateTimeCreat'
-      },
-      {
-        title: '使用时间',
-        key: 'dateTimeUse'
-      }
-    ];
-    data= [
-      {
-        key: '1',
-        value: '1',
-        admin: '1',
-        dateTimeCreat: '1',
-        dateTimeUse: '1'
-      }
-    ];
-    loading= false;
-    pagination= {
-      currentPage: 1,
-      pageSize: 5,
-      total: 0
-    };
-    options= {
-      border: true
-    };
-    rowHandle= {
-      custom: [
-        {
-          text: '自定义按钮',
-          type: 'warning',
-          size: 'small',
-          emit: 'custom-emit-1'
-        }
+  @Component({
+    components: {
+      PieDemo
+    }
+  })
+  export default class Sale extends Vue {
+    list = [{
+      value: '选项1',
+      label: '黄金糕'
+    }, {
+      value: '选项2',
+      label: '双皮奶'
+    }, {
+      value: '选项3',
+      label: '蚵仔煎'
+    }, {
+      value: '选项4',
+      label: '龙须面'
+    }, {
+      value: '选项5',
+      label: '北京烤鸭'
+    }];
+
+    value8 = '';
+
+    chartData = {
+      columns: ['日期', '访问用户'],
+      rows: [
+        { '日期': '1/1', '访问用户': 1393 },
+        { '日期': '1/2', '访问用户': 3530 },
+        { '日期': '1/3', '访问用户': 2923 },
+        { '日期': '1/4', '访问用户': 1723 },
+        { '日期': '1/5', '访问用户': 3792 },
+        { '日期': '1/6', '访问用户': 4593 }
       ]
     };
+
     mounted () {
-    }
-    handleCustomEvent ({index, row}) {
-      this.$message.success(index.toString())
-      console.log(index)
-      console.log(row)
     }
   }
 </script>
