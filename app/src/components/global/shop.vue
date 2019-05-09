36fad189b87e438d628393393dac8941aa5c4515
TulipaGesneriana <503026694@qq.com>
2019-05-09 12:46:10 +0800

    app bug修复

--- app/src/components/global/shop.vue
+++ app/src/components/global/shop.vue
@@ -46,27 +46,27 @@
           </i>
           至
           <i>
-            {{ `￥${item.minAmount?item.minAmount.toFixed(2):0}` }}
+            {{ `￥${item.maxAmount?item.maxAmount.toFixed(2):0}` }}
           </i>
         </span>
       </div>
-      <!--<div-->
-        <!--v-show="item.minMedicaidAmount && item.maxMedicaidAmount"-->
-        <!--class="line line5"-->
-      <!--&gt;-->
-        <!--<span>-->
-          <!--医保区间：-->
-        <!--</span>-->
-        <!--<span>-->
-          <!--<i>-->
-            <!--{{ `￥${item.minMedicaidAmount.toFixed(2)}` }}-->
-          <!--</i>-->
-          <!--至-->
-          <!--<i>-->
-            <!--{{ `￥${item.maxMedicaidAmount.toFixed(2)}`}}-->
-          <!--</i>-->
-        <!--</span>-->
-      <!--</div>-->
+      <div
+        v-show="item.minMedicaidAmount && item.maxMedicaidAmount"
+        class="line line5"
+      >
+        <span>
+          医保区间：
+        </span>
+        <span>
+          <i>
+            {{ `￥${item.minMedicaidAmount.toFixed(2)}` }}
+          </i>
+          至
+          <i>
+            {{ `￥${item.maxMedicaidAmount.toFixed(2)}`}}
+          </i>
+        </span>
+      </div>
       <div class="line7">
         <!--<span v-if="item.distribution === true" class="tag">-->
           <!--可配送-->
